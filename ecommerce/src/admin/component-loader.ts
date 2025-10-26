d983b2cc38385ffce4c8159a69dd2055f459f5fa
Sangeerth2001 <ysangeerththanasarma@gmail.com>
2025-10-26 20:07:22 +0530

    Remove non-existent UserDashboard component from loader

--- ecommerce/src/admin/component-loader.ts
+++ ecommerce/src/admin/component-loader.ts
@@ -10,7 +10,6 @@ const componentLoader = new ComponentLoader();
 // Register custom components
 const componentsDir = path.join(__dirname, 'components');
 componentLoader.add('AdminDashboard', path.join(componentsDir, 'AdminDashboard'));
-componentLoader.add('UserDashboard', path.join(componentsDir, 'UserDashboard'));
 componentLoader.add('RoleDashboard', path.join(componentsDir, 'RoleDashboard'));
 componentLoader.add('SettingsPage', path.join(componentsDir, 'SettingsPage'));
 componentLoader.add('UserSettings', path.join(componentsDir, 'UserSettings'));
